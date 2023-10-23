5ee61544acfc03f2be29a27d27dc95b8f8fab9e8
Lorenzo Cominelli <107864420+ilgiostraio@users.noreply.github.com>
2023-10-23 17:17:37 +0200

    added the run batch file

---
+++ run.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+
+C:\Users\abel\AppData\Local\Microsoft\WindowsApps\Microsoft.WindowsTerminal_8wekyb3d8bbwe\wt.exe new-tab  --title "ROS CORE" ; split-pane --title "Abel Control" --tabColor #008000 ; move-focus left ; split-pane -H --title "Dynamixel Control" --tabColor #8B0000 
